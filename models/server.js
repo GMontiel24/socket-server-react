1f493f380a4b5bd316add48997f36589abc9fcae
German Hugo Montiel <german_montiel96@hotmail.com>
2022-04-05 01:01:23 -0300

    refactor index.html

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
 
@@ -21,6 +22,9 @@ class Server {
     middlewares() {
         // Desplegar directorio publico
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        //CORS
+        this.app.use(cors());
     }
 
     configuracionSockets() {
